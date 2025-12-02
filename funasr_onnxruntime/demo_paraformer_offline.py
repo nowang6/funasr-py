45ddf868221b167e7d657d2cdd99083e2d87770a
nowang6 <norman6.wang@gmail.com>
2025-12-02 18:54:54 +0800

    update

--- funasr_onnxruntime/demo_paraformer_offline.py
+++ funasr_onnxruntime/demo_paraformer_offline.py
@@ -1,15 +1,16 @@
 from funasr_onnx import Paraformer
 from pathlib import Path
 
-model_dir = "damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
+model_dir = "models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online-onnx"
 # model_dir = "damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
-model = Paraformer(model_dir, batch_size=1, quantize=False)
+model = Paraformer(model_dir, batch_size=1, quantize=True)
 # model = Paraformer(model_dir, batch_size=1, device_id=0)  # gpu
 
 # when using paraformer-large-vad-punc model, you can set plot_timestamp_to="./xx.png" to get figure of alignment besides timestamps
 # model = Paraformer(model_dir, batch_size=1, plot_timestamp_to="test.png")
 
-wav_path = ["{}/.cache/modelscope/hub/{}/example/asr_example.wav".format(Path.home(), model_dir)]
+wav_path = ["data/张三丰.wav"]
 
 result = model(wav_path)
 print(result)
+
